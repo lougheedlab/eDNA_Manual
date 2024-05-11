6b224e0eb255498e4ef2eb322227b643de6653ca
David Lougheed <david.lougheed@gmail.com>
2024-05-11 16:16:34 -0400

    configure figure numbering

--- src/conf.py
+++ src/conf.py
@@ -27,6 +27,9 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+#    - Assign numbers to figures
+numfig = True
+
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
